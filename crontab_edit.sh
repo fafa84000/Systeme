9206dcaa3fa8379d507ab816425ba5fb61d451e2
Farouk Assim <uapv2306164@FriFroukDesktop.>
2025-04-20 22:32:36 +0200

    D

---
+++ crontab_edit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Avez-vous verifié le contenu de crontab avant de lancer ce script ? (Y/n)" choix
+
+choix=${choix,,}
+
+if [[ "$choix" == "y" || "$choix" == "" ]]; then
+	crontab > /tmp/crontab_tmp.txt
+	cat crontab.txt >> /tmp/crontab_tmp.txt
+	crontab /tmp/crontab_tmp.txt
+	echo "Contenu de \"crontab.txt\" copié dans crontab."
+else
+	echo "pour modifer crontab faire: crontab -e"
+	echo "pour afficher le contenu de crontab faire: crontab -l"
+fi
